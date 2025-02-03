8a04db1a34571156d70775f8a4ceb90ec637d903
minnu2204 <tadikonda.srivardhini.s81@kalvium.community>
2025-02-03 14:57:20 +0530

    Add files via upload

---
+++ first_API/making_your_first_api_and_page_assignment_1/server.js
@@ -0,0 +1,48 @@
+// Boilerplate Code for Virtual Assistant API
+const express = require('express');
+const app = express();
+
+function message(){
+
+  let date =new Date()
+  let day =date.getDay()
+
+  if(day==1){
+    return {
+      "dayMessage": "Happy Monday! Start your week with energy!"
+    }
+  }
+  else if(day==5){
+    return{
+      "dayMessage": "It's Friday! The weekend is near!"
+    }
+  }
+  else{
+    return{
+      "dayMessage": "Have a wonderful day!"
+    }
+  }
+}
+
+
+
+app.get("/assistant/greet",async(req,res)=>{
+   try {
+      let user =req.query.name
+      console.log(user)
+       let mess = message()
+       console.log(mess)
+      let welcomeMessage =`"Hello, ${user} Welcome to our assistant app!"`
+      mess.welcomeMessage =welcomeMessage
+      res.status(200).json(mess)
+    
+   } catch (error) {
+       res.status(500).json({status:false,message:"internal server error"})
+   }
+})
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Virtual Assistant API is running on http://localhost:${PORT}`);
+});
